1fcf4b5b22b9e46594871229f97e5642e098a8c4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# How to customize standard ASP.NET controls to look similar to DevExpress controls
+
+
+<p>Sometimes, it is required to combine standard ASP.NET controls with our DevExpress controls. However, it is difficult to customize the appearance of standard controls. <br>This example demonstrates how to implement this and create controls that look like DevExpress controls that use the Moderno theme. <br><br>For control customizing, add the required styles and scripts to your page:</p>
+
+
+```aspx
+<link href="Styles/Moderno/theme.css" rel="stylesheet" />
+<script src="Styles/Moderno/moderno.js"></script>
+
+```
+
+
+<p> Then, just set the CssClass property of your control to "Moderno". For example:</p>
+
+
+```aspx
+<asp:Button ID="Button3" runat="server" Text="ASP Button" CssClass="Moderno" />
+
+```
+
+
+<p>For some controls, it is necessary to ad an additional CSS classes:</p>
+
+
+```aspx
+<asp:CheckBoxList ID="CheckBoxList1" runat="server" CssClass="Moderno Moderno_CheckBoxList">
+	...
+</asp:CheckBoxList>
+<asp:Calendar ID="Calendar2" runat="server" CssClass="Moderno Moderno_Calendar" Enabled="false">
+	<TitleStyle CssClass="Moderno_CalendarTitle" />
+</asp:Calendar>
+
+```
+
+
+<p> This way, your controls will be patched so you can customize their appearance using CSS. All required CSS rules used in this examples are placed in the <strong><em>Styles/Moderno/theme.css</em></strong> file. </p>
+<p><br>If you want to change any styles, you can inspect CSS rules at your end. Refer to the <a href="https://www.devexpress.com/Support/Center/p/K18570">How to inspect CSS rules</a> thread for more information about this option.</p>
+
+<br/>
+
+
---
+++ VB/Styles/Moderno/moderno.js
@@ -0,0 +1,102 @@
+﻿$(document).ready(function () {
+    //CheckBox
+    var uncheckedAspNetCheckBox = $(".Moderno input:checkbox:not(:checked):not(:disabled)");
+    uncheckedAspNetCheckBox.hide();
+    uncheckedAspNetCheckBox.after("<div tabindex='0' class='ModernoCheckBox ModernoCheckBoxUnchecked'/>");
+
+    var checkedAspNetCheckBox = $(".Moderno input:checkbox:checked:not(:disabled)");
+    checkedAspNetCheckBox.hide();
+    checkedAspNetCheckBox.after("<div tabindex='0' class='ModernoCheckBox ModernoCheckBoxChecked'/>");
+
+    var uncheckedAspNetCheckBoxDisabled = $(".Moderno input:checkbox:not(:checked):disabled");
+    uncheckedAspNetCheckBoxDisabled.hide();
+    uncheckedAspNetCheckBoxDisabled.after("<div class='ModernoCheckBox ModernoCheckBoxUncheckedDisabled'/>");
+
+    var checkedAspNetCheckBoxDisabled = $(".Moderno input:checkbox:checked:disabled");
+    checkedAspNetCheckBoxDisabled.hide();
+    checkedAspNetCheckBoxDisabled.after("<div class='ModernoCheckBox ModernoCheckBoxCheckedDisabled'/>");
+
+    $('.ModernoCheckBox').click(function () {
+        var $image = $(this);
+        var $checkbox = $(this).prev("input");
+        if ($checkbox.is(':disabled'))
+            return;
+
+        $checkbox.attr('checked', !$checkbox.attr('checked'));
+        $image.toggleClass("ModernoCheckBoxChecked");
+        $image.toggleClass("ModernoCheckBoxUnchecked");
+    })
+
+    //Calendar
+    $(".Moderno.Moderno_Calendar").attr("cellpadding", "4");
+    var calendarTitleContainer = $(".Moderno_CalendarTitle").parent();
+    calendarTitleContainer.addClass("ModernoCalendarTitleContainer");
+    calendarTitleContainer.attr("style", "");
+
+    var calendarTitleNextButton = calendarTitleContainer.find("a:eq(1)");
+    if (calendarTitleNextButton)
+        calendarTitleNextButton.text('___');
+    calendarTitleNextButton.addClass("Moderno_edtCalendarNextMonth");
+
+
+    var calendarTitlePrevButtonContainer = calendarTitleContainer.find("td:eq(0)");
+
+    calendarTitlePrevButtonContainer.each(function () {
+        var calendarTitlePrevButton = $(this).find("a");
+        if (calendarTitlePrevButton.length > 0) {
+            calendarTitlePrevButton.text('___');
+            calendarTitlePrevButton.addClass("Moderno_edtCalendarPrevMonth");
+        }
+        else {
+            $(this).text('');
+            $(this).append("<div class='Moderno_edtCalendarPrevMonthDisabled'/>");
+        }
+    })
+
+    var calendarTitleNextButtonContainer = calendarTitleContainer.find("td:eq(2)");
+
+    calendarTitleNextButtonContainer.each(function () {
+        var calendarTitleNextButton = $(this).find("a");
+        if (calendarTitleNextButton.length > 0) {
+            calendarTitleNextButton.text('___');
+            calendarTitleNextButton.addClass("Moderno_edtCalendarNextMonth");
+        }
+        else {
+            $(this).text('');
+            $(this).append("<div class='Moderno_edtCalendarNextMonthDisabled'/>");
+        }
+    })
+
+    //Radioutton
+    var uncheckedAspNetRadioButton = $(".Moderno input:radio:not(:checked):not(:disabled)");
+    uncheckedAspNetRadioButton.hide();
+    uncheckedAspNetRadioButton.after("<div tabindex='0' class='ModernoRadioButton ModernoRadioButtonUnchecked'/>");
+
+    var checkedAspNetRadioButton = $(".Moderno input:radio:checked:not(:disabled)");
+    checkedAspNetRadioButton.hide();
+    checkedAspNetRadioButton.after("<div tabindex='0' class='ModernoRadioButton ModernoRadioButtonChecked'/>");
+
+    var uncheckedAspNetRadioButtonDisabled = $(".Moderno input:radio:not(:checked):disabled");
+    uncheckedAspNetRadioButtonDisabled.hide();
+    uncheckedAspNetRadioButtonDisabled.after("<div class='ModernoRadioButton ModernoRadioButtonUncheckedDisabled'/>");
+
+    var checkedAspNetRadioButtonDisabled = $(".Moderno input:radio:checked:disabled");
+    checkedAspNetRadioButtonDisabled.hide();
+    checkedAspNetRadioButtonDisabled.after("<div class='ModernoRadioButton ModernoRadioButtonCheckedDisabled'/>");
+
+    $('.ModernoRadioButton').click(function () {
+        var $image = $(this);
+        var $radioBtn = $(this).prev("input");
+        if ($radioBtn.is(':disabled'))
+            return;
+
+        $radioBtnInGroup = $("input[name=" + $radioBtn.attr('name') + "]");
+        $radioBtnInGroup.attr('checked', false);
+        $radioBtn.attr('checked', !$radioBtn.attr('checked'));
+
+        $radioBtnInGroup.next("div").removeClass("ModernoRadioButtonChecked");
+        $radioBtnInGroup.next("div").addClass("ModernoRadioButtonUnchecked");
+        $image.toggleClass("ModernoRadioButtonChecked");
+        $image.toggleClass("ModernoRadioButtonUnchecked");
+    })
+});
